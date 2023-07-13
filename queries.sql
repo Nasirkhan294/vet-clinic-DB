07d032888f03920548edb1f0853c50953a288d0a
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-07-13 19:28:03 +0500

    Add queries to update the table

--- queries.sql
+++ queries.sql
@@ -117,3 +117,28 @@ SELECT
 	*
 FROM
 	animals;
+
+BEGIN;
+
+DElETE FROM
+	animals
+WHERE
+	date_of_birth > '2022-01-01';
+
+SAVEPOINT my_savepoint;
+
+UPDATE
+	animals
+SET
+	weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT my_savepoint;
+
+UPDATE
+	animals
+SET
+	weight_kg = weight_kg * -1
+WHERE
+	weight_kg < 0;
+
+COMMIT;
